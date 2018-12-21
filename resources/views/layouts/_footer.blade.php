5c2bca2931c3ab0cdcf1bd4ea65109e84ee2808c
steven.su <b9813147@gmail.com>
2018-12-21 16:46:19 +0800

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            由 <a href="#">ares</a>設計編輯 <span style="color: #e27575;font-size: 14px;">^^</span>
+        </p>
+        <p class="float-right"><a href="#">聯絡我們</a></p>
+    </div>
+</footer>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- CSRF Token --}}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title', 'laraBBS') - laravel5.7 進階測試</title>
+    {{-- Styles --}}
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+<div id="app" class="{{ route_class() }}-page">
+    @include('layouts._header')
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    @include('layouts._footer')
+
+    {{-- Script --}}
+    <script src="{{ asset('js/app.js') }}"></script>
+</div>
+</body>
+</html>
